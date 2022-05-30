2508a44cafee5dfc24314eb340187a2adb897d6f
sataverse <odrimung2@gmail.com>
2022-05-30 21:40:11 +0900

    2022-05-30 Commit

---
+++ src/main/java/crwft/crwftserver/CrwftServerApplication.java
@@ -0,0 +1,13 @@
+package crwft.crwftserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrwftServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrwftServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crwft/crwftserver/CrwftServerApplicationTests.java
@@ -0,0 +1,13 @@
+package crwft.crwftserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrwftServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
